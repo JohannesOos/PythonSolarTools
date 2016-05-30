4c248ef766d20e8094d49852c0fba4114fa01be4
Johannes Oos <j.oos@hotmail.de>
2016-05-30 07:33:26 +0300

    add function for load profile adjustment

--- Load_Profile_Match.py
+++ Load_Profile_Match.py
@@ -35,5 +35,37 @@ for row_index in rowsWithData:
 
 if len(E_Solar) == 8760:
     print 'One year hourly read in'
-#print E_Load
+else:
+    print 'Check length of Solar file'
+
+   
+#use this to adjust file
+#Option 1 is values in different resolution for the entire year
+def x_to_hourly_entire_year(E_Load):
+    """ E_Load: List of load value for entire year
+    resolution is multiple of one hour """
+    goal_length = 8760
+    ratio =  goal_length / len(E_Load)
+    new_profile = []
+    for a in range(len(E_Load)):
+        for b in range(ratio):
+            new_profile.append(E_Load[a])
+    return new_profile
+    
+#Option2 is if there are not enough values    
+def x_to_yearly(E_Load, resolution):
+    """
+    E_Load: List of production value starting and ending with midnight
+    resolution: resolution of values in minutes Must be divisor of 1 hour
+    """
+    goal_length = 8760 *60/resolution
+    ratio =  goal_length / len(E_Load)
+    new_profile = []
+    for a in range(ratio):
+        for b in range(len(E_load)):
+            new_profile.append(E_Load[b])
+    return new_profile
+    
+
+
 
